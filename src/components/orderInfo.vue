bd947f4b4cfa9029cb02104caea4e7b14ba91fad
CoffeerLatte <todty2312@gmail.com>
2024-02-06 14:36:09 +0900

    주문서 정보 조회
    - [x] Add orderInfo.vue
    - [x] modified App.vue

---
+++ src/components/orderInfo.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const jsonData = ref([])
+
+async function fetchData() {
+  try {
+    const response = await fetch("http://localhost:3000/db");
+    jsonData.value = await response.json();
+    console.log(jsonData.value);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+onMounted(() => {
+  fetchData();
+})
+</script>
+
+<template>
+  <div>
+    <p v-for="item in jsonData" :key="item.id">
+      {{ item }}
+    </p>
+  </div>
+</template>
+
