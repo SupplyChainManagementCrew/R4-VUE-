4d9538862797e9138b12da0d4939831e4b0dfa63
dhkdtld37 <149128094+dhkdtld37@users.noreply.github.com>
2024-02-06 14:15:39 +0900

    Update Most.vue

--- src/components/Most.vue
+++ src/components/Most.vue
@@ -1,32 +1,15 @@
 <script setup>
+import product from "@/assets/product.json"
+const products = product
 
-import { ref, onMounted } from 'vue';
-
-const products = ref([]);
-
-const mostSoldProduct = ref({ name: '', sales: 0 });
-
-onMounted(() => {
-  fetch('@/assets/products.json')
-    .then(response => response.json())
-    .then(data => {
-      products.value = data;
-      updateMostSoldProduct();
-    });
-});
-
-function updateMostSoldProduct() {
-  mostSoldProduct.value = products.value.reduce(
-    (prev, current) => (prev.sales > current.sales ? prev : current),
-    {}
-  );
-}
 </script>
 
 <template>
   <div>
     <h2>Most Sold Product</h2>
-    <p>{{ mostSoldProduct.name }} - {{ mostSoldProduct.sales }} units</p>
+     <li v-for="item in products">
+	    {{ item.name }} - {{ item.sales }} 
+     </li>
   </div>
 </template>
 
