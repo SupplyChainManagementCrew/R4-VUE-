045066e958c2b5ff9f530fc001ba00266f0d05fc
donghyuk <ldh0308@naver.com>
2024-02-06 17:00:39 +0900

    templte

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
-<script setup>
+<template>
+  <v-app>
+    <v-main>
+      <Delivery/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
 import Delivery from './components/Delivery.vue'
 
+export default {
+  name: 'App',
+components: {
+  Delivery
+}
+}
 </script>
-
-<template>
-  <Delivery/>
-</template>
