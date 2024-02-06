e33d9fe94c0965275446da888c65e60a72f244e1
parc02 <pascalmin49@gmail.com>
2024-02-06 13:45:49 +0900

    Graph vue에 출력 <template> <form class="d-flex" role="search">    <input class="form-control me-2" v-model="searchQuery" placeholder="타이틀을 입력하세요" @input="search"/>     </form><br>     <ul class="list-group">       <li class="list-group-item" v-for="post in filteredPosts" :key="post.id">       Id: {{ post.id }} - Title: {{ post.title }} - Views: {{ post.views }}</li>     </ul> </template>

---
+++ src/components/graph-ms.vue
@@ -0,0 +1,40 @@
+<template> 
+       <li v-for="item in graph">
+        <p>Date: {{ item.date }}</p>
+        <p>Revenue: {{ item.revenue }}</p>
+        <p>Sales Quantity: {{ item.sales_quantity }}</p>
+        <p>Recipient: {{ item.recipient }}</p>
+        <p>{{item}}</p>
+      </li>
+</template>
+
+
+<script setup>
+import { ref, onMounted, watch } from 'vue';
+// import axios from 'axios';
+
+const searchQuery = ref(''); //양방향 검색 담을 그릇
+const graph = ref([]); // json 값 담을 그릇
+// const filteredPosts = ref([]); // 출력 값 담을 그릇
+
+async function fetchData() {
+    graph.value = null
+        const res = await fetch(
+                `https://r1-json-server.fly.dev/graph`
+                )
+        graph.value = await res.json()
+}
+
+// const filterPosts = () => {
+//   if (searchQuery.value) {
+//     filteredPosts.value = posts.value.filter(post => post.recipient_address .includes(searchQuery.value));
+//   } else {
+//     filteredPosts.value = posts.value;
+//   }
+// };
+
+onMounted(() => {
+  fetchData();
+});
+
+</script>
