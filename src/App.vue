a99021c0dae34858dd6a50486c959ed5834d10ca
dhkdtld37 <hyeonnls96@naver.com>
2024-02-07 10:29:11 +0900

    delete

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,58 @@
 <template>
   <v-app>
     <v-main>
-      <Delivery/>
-      <orderinfo/>
-      <Most/>
-      <graphMs/>
-      <Comparison />
+      <v-container fluid>
+        <v-row>
+          <v-col>
+            <v-card>
+              <v-card-title>Delivery</v-card-title>
+              <v-card-text>
+                <Delivery />
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card>
+              <v-card-title>Order Info</v-card-title>
+              <v-card-text>
+                <orderinfo />
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-card>
+              <v-card-title>Most Sold Product</v-card-title>
+              <v-card-text>
+                <Most />
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card>
+              <v-card-title>Graph</v-card-title>
+              <v-card-text>
+                <graphMs />
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-card>
+              <v-card-title>Comparison</v-card-title>
+              <v-card-text>
+                <Comparison />
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
+
 
 <script>
 import Delivery from './components/Delivery.vue'
@@ -16,14 +60,15 @@ import orderinfo  from './components/orderInfo.vue'
 import Most from './components/Most.vue';
 import graphMs from './components/graph-ms.vue'
 import Comparison from './components/Comparison.vue'
+
 export default {
   name: 'App',
-components: {
-  Delivery,
-  orderinfo,
-  Most,
-  graphMs,
-  Comparison
-}
+  components: {
+    Delivery,
+    orderinfo,
+    Most,
+    graphMs,
+    Comparison
+  }
 }
 </script>
